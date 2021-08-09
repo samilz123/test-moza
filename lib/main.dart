23618cadea74d7a65e0396175f8ab9a01801a6e1
samilz123 <lazhar.sami1@gmail.com>
2021-08-09 22:23:08 +0100

    answer added with error

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './question.dart';
+import './answer.dart';
 
 /*void main() {
   runApp(MyApp());
@@ -29,8 +30,8 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     var questions = [
-      "what's your favorite color",
-      'what\'s your favorite animals',
+      "What's your favorite color",
+      'What\'s your favorite animals',
     ];
     return MaterialApp(
       home: Scaffold(
@@ -40,20 +41,9 @@ class _MyAppState extends State<MyApp> {
         body: Column(
           children: [
             Question(questions[_questionIndex]),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: () => print('Answer 2 chosen!'),
-            ),
-            RaisedButton(
-              child: Text('Answer 3'),
-              onPressed: () {
-                print("Answer 3 chosen!");
-              },
-            ),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
           ],
         ),
       ),
