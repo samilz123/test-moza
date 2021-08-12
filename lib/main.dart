6c92a8baa489ab13f5b3bde929c011c4274a9e21
samilz123 <lazhar.sami1@gmail.com>
2021-08-12 23:48:25 +0100

    added quiz + result widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
+import 'package:first_app/result.dart';
 import 'package:flutter/material.dart';
-import './question.dart';
-import './answer.dart';
-
+import './quiz.dart';
+import 'result.dart';
 /*void main() {
   runApp(MyApp());
 }*/
@@ -17,7 +17,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final List questions = const [
+  final List<Map<String, Object>> _questions = const [
     {
       'questionText': "What's your favorite color?",
       'answers': ['Black', 'Red', 'Green', 'White'],
@@ -40,7 +40,7 @@ class _MyAppState extends State<MyApp> {
     });
     //print('Answer chosen!');
     print(_questionIndex);
-    if (_questionIndex < questions.length) {
+    if (_questionIndex < _questions.length) {
       print('We have more questions!');
     } else
       print('finished!');
@@ -53,21 +53,13 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('My first appp'),
         ),
-        body: _questionIndex < questions.length
-            ? Column(
-                children: [
-                  Question(
-                    questions[_questionIndex]['questionText'],
-                  ),
-                  ...(questions[_questionIndex]['answers'] as List<String>)
-                      .map((answer) {
-                    return Answer(_answerQuestion, answer);
-                  }).toList()
-                ],
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
               )
-            : Center(
-                child: Text('No more questions habibi!'),
-              ),
+            : Result(),
       ),
     );
   }
